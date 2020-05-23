5a5f4f87db9037eb557d24135161cb2d56dbc7a8
Rahul <ra.goel1995@gmail.com>
2020-05-23 14:32:53 +0530

    curring

---
+++ 8_curring.js
@@ -0,0 +1,20 @@
+// let multiply = function (x, y) {
+//   console.log(x * y);
+// };
+
+// let multiplyByTwo = multiply.bind(this, 2);
+// multiplyByTwo(5);
+
+// let multiplyByThree = multiply.bind(this, 3);
+// multiplyByThree(5);
+
+//Closure
+
+let multiply = function (x) {
+  return function (y) {
+    console.log(x * y);
+  };
+};
+
+let multiplyByTwo = multiply(2);
+multiplyByTwo(5);
